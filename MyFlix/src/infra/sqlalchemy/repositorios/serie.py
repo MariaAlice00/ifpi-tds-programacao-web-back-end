d3bd774a8f0b455483076f824108fdbccc3fa89e
MariaAlice00 <mariaalice021200@gmail.com>
2021-04-20 16:42:47 -0300

    finished!

--- MyFlix/src/infra/sqlalchemy/repositorios/serie.py
+++ MyFlix/src/infra/sqlalchemy/repositorios/serie.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from src.schemas import schemas
 from src.infra.sqlalchemy.models import models
 
+
 class RepositorioSerie():
     def __init__(self, db: Session): # db do tipo Session
         self.db = db
@@ -21,3 +22,21 @@ class RepositorioSerie():
         series = self.db.query(models.Serie).all() # query: consultar
         return series
 
+
+    def exibir_id(self, id):
+        lista_id = self.db.query(models.Serie).filter(models.Serie.id == id).first()
+        return lista_id
+
+
+    def exibir_titulo(self, titulo):
+        lista_titulo = self.db.query(models.Serie).filter(models.Serie.titulo == titulo).first()
+        return lista_titulo
+
+
+    def remover(self, id):
+        lista_remover = self.db.query(models.Serie).filter(models.Serie.id == id).first()
+        
+        self.db.delete(lista_remover)
+        self.db.commit()
+
+        return 'Série removida'
--- MyFlix/src/server.py
+++ MyFlix/src/server.py
@@ -4,6 +4,7 @@ from src.schemas.schemas import Serie
 from src.infra.sqlalchemy.config.database import get_db, criar_bd
 from src.infra.sqlalchemy.repositorios.serie import RepositorioSerie
 
+
 criar_bd()
 
 
@@ -21,3 +22,22 @@ def criar_serie(serie: Serie, db: Session = Depends(get_db)):
 def listar_series(db: Session = Depends(get_db)):
     series  = RepositorioSerie(db).listar()
     return series
+
+
+@app.get('/series/{id}')
+def listar_id(id: str, db: Session = Depends(get_db)):
+    series_id = RepositorioSerie(db).exibir_id(id)
+    return series_id
+    # se o id não existe, retorna null
+
+
+@app.put('/series/{titulo}')
+def listar_titulo(titulo: str, db: Session = Depends(get_db)):
+    series_titulo = RepositorioSerie(db).exibir_titulo(titulo)
+    return series_titulo
+
+
+@app.delete('/series/{id}')
+def remover_serie(id: str, db: Session = Depends(get_db)):
+    series_removidas = RepositorioSerie(db).remover(id)
+    return series_removidas
